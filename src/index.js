9fbed14a2ff3f51c15fc40235c6cb38e788db483
Miguel Passos <miguel.alves@procfit.com.br>
2026-04-10 11:15:38 -0300

    Configurando com tsup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 //Exemplo Codigo
+export function soma(a, b) {
+  return a + b;
+}
+
+export default { soma };
+
+/*  
+    libs para deixar sua lib usavel em ES Module e CommonJS
+        rollup: para libs maiores
+        tsup: para aprender a criar
+*/
